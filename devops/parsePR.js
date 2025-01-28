e35ddc6721afebf27773b5d9a7f38ebaf4d9c05f
smartinello1 <martinellosimone01@gmail.com>
2025-01-28 17:09:48 +0100

    Update parsePR.js

--- devops/parsePR.js
+++ devops/parsePR.js
@@ -1,7 +1,11 @@
 const glob = require('@actions/glob');
 
-const patterns = ['./devops/changed-sources/**.cls']
-const globber = await glob.create(patterns.join('\n'))
-const files = await globber.glob()
+async function start() {
+  const patterns = ['./devops/changed-sources/**.cls']
+  const globber = await glob.create(patterns.join('\n'))
+  const files = await globber.glob()
+  
+  console.log('files: ' , files)
+}
 
-console.log('files: ' , files)
+start()
