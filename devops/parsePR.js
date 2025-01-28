00e533eb958fa341f40fa91ea9aaa562433b9431
smartinello1 <martinellosimone01@gmail.com>
2025-01-28 16:45:42 +0100

    Update parsePR.js

--- devops/parsePR.js
+++ devops/parsePR.js
@@ -1,26 +1,7 @@
-const fs = require('fs');
-const readline = require('readline');
+const glob = require('@actions/glob');
 
-async function extractTests() {
-	//by default we specify that all tests should run
-	let testsFile = __dirname + '/devops/testsToRun.txt';
-	await fs.promises.writeFile(testsFile, 'all');
+const patterns = ['./devops/changed-sources/**.cls']
+const globber = await glob.create(patterns.join('\n'))
+const files = await globber.glob()
 
-	const lines = readline.createInterface({
-		input: fs.createReadStream(__dirname + '/pr_body.txt'),
-		crlfDelay: Infinity
-	});
-
-	for await (const line of lines) {
-		//special delimeter for apex tests
-		if (line.includes('Apex::[') && line.includes(']::Apex')) {
-			let tests = line.substring(8, line.length - 7);
-			await fs.promises.writeFile(testsFile, tests);
-			await fs.promises.appendFile(testsFile, '\n');
-		}
-	}
-
-	console.log('test')
-}
-
-extractTests();
+console.log('files: ' , files)
